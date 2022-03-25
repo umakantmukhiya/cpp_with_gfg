64a349faf7b083a545646ed985a2748235250184
Umakant Mukhiya <42721219+umakantmukhiya@users.noreply.github.com>
2022-03-25 17:17:51 +0530

    Update reverseString.cpp

--- reverseString.cpp
+++ reverseString.cpp
@@ -23,3 +23,36 @@ int main(){
 
 	return 0;
 }
+
+---------------------above code takes O(|S|) Auxilary Space-------------------------------
+	
+#include <bits/stdc++.h>
+using namespace std;
+
+string strstr(string str){
+	 
+	char temp;
+	int len = str.length();
+	for(int i = 0; i < (len/2); i++){
+		 temp = str[i];
+		 str[i] = str[len-1-i];
+		 str[len-1-i] = temp;
+	}
+
+	return str;
+}
+
+int main(){
+
+	freopen("input.txt","r",stdin);
+
+	string s1;
+	cin >> s1;
+
+	cout << strstr(s1);
+
+
+	return 0;
+}
+
+---------------------above code takes O(1) Auxilary Space-------------------------------
