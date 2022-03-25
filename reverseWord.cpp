b2278b5b4488e907641c9f70519260cc1648ed03
Umakant Mukhiya <42721219+umakantmukhiya@users.noreply.github.com>
2022-03-26 00:30:03 +0530

    Update reverseWord.cpp

--- reverseWord.cpp
+++ reverseWord.cpp
@@ -23,3 +23,37 @@
 	return si;
 	
     }
+
+-------------------------------------vesrion 2---------------------------------------------------------
+	
+	#include <bits/stdc++.h>
+using namespace std;
+
+void strstr(string str){
+	 
+	int len = str.length();
+	char *p = new char[len+1];
+
+	strcpy(p,str.c_str());
+
+	for(int i = len-1; i >= 0 ; i--){
+		if(p[i] == ' '){
+			p[i] = '\0';
+			printf("%s ",p+i+1);
+		}
+
+		if(i == 0){
+			printf("%s",p+i);
+		}
+	}
+}
+
+int main(){
+
+	freopen("input.txt","r",stdin);
+
+	string s1;
+	getline(cin,s1);
+	strstr(s1);
+	return 0;
+}
